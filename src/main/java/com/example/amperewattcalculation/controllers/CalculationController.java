c31a68012a627d20ce80308609b3773aa21bcfe3
kensyu1000 <h95342626@gmail.com>
2023-08-07 15:19:11 +0900

    ボタンにidを付与させてページを開ける

--- src/main/java/com/example/amperewattcalculation/controllers/CalculationController.java
+++ src/main/java/com/example/amperewattcalculation/controllers/CalculationController.java
@@ -1,18 +1,35 @@
 package com.example.amperewattcalculation.controllers;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.amperewattcalculation.models.PowerStrip;
+import com.example.amperewattcalculation.repository.PowerStripRepository;
 
 @Controller
 public class CalculationController {
+
+  // PowerStripクラスのフィールドをfinalにする
+  private final PowerStripRepository repository;
+
+  public CalculationController(PowerStripRepository repository) {
+    this.repository = repository;
+  }
+
   @GetMapping("/")
   public String calculation() {
     return "calculation";
   }
 
   @GetMapping("/device")
-  public String device() {
+  public String device(@ModelAttribute PowerStrip powerstrip, Model model) {
+    model.addAttribute("powerstrips", repository.findAll());
     return "device";
   }
 
@@ -21,8 +38,22 @@ public class CalculationController {
     return "PowerStrip/register";
   }
 
-  @GetMapping("/powerstrip/edit")
-  public String ps_edit() {
+  @PostMapping("/powerstrip/register")
+  public String ps_register(@ModelAttribute PowerStrip powerstrip) {
+    return "device";
+  }
+
+  @GetMapping("/powerstrip/edit/{id}")
+  public String ps_edit(@PathVariable("id") long ps_id, Model model) {
+    // idと一致するデータを取得
+    model.addAttribute("powerstrip", repository.findById(ps_id));
+    return "PowerStrip/edit";
+  }
+
+  @PostMapping("/powerstrip/edit/{id}")
+  public String ps_edit2(@PathVariable("id") long ps_id, Model model) {
+    // idと一致するデータを取得
+    model.addAttribute("powerstrip", repository.findById(ps_id));
     return "PowerStrip/edit";
   }
 
@@ -34,5 +65,23 @@ public class CalculationController {
   @GetMapping("/item/edit")
   public String item_edit() {
     return "Item/edit";
+  }
+
+  // 初期データ
+  @PostConstruct
+  public void dataInit() {
+    PowerStrip Panasonic = new PowerStrip();
+    Panasonic.setPs_maker_name("Panasonic");
+    Panasonic.setPs_name("電源タップ");
+    Panasonic.setPs_code("P01");
+    Panasonic.setOutllet_number(3);
+    repository.saveAndFlush(Panasonic);
+
+    PowerStrip Apw = new PowerStrip();
+    Apw.setPs_maker_name("Apw");
+    Apw.setPs_name("電源タップ");
+    Apw.setPs_code("APW01");
+    Apw.setOutllet_number(3);
+    repository.saveAndFlush(Apw);
   }
 }
--- src/main/java/com/example/amperewattcalculation/repository/PowerStripRepository.java
+++ src/main/java/com/example/amperewattcalculation/repository/PowerStripRepository.java
@@ -1,5 +1,9 @@
 package com.example.amperewattcalculation.repository;
 
-public interface PowerStripRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.amperewattcalculation.models.PowerStrip;
+
+public interface PowerStripRepository extends JpaRepository<PowerStrip, Long> {
 
 }
