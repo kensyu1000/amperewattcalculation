ecc36d99cfdd2f8910fcbcbbe665b34e2170cceb
kensyu1000 <h95342626@gmail.com>
2023-08-08 15:39:59 +0900

    controllerの分解完了

---
+++ src/main/java/com/example/amperewattcalculation/controllers/ItemController.java
@@ -0,0 +1,74 @@
+package com.example.amperewattcalculation.controllers;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.amperewattcalculation.models.Item;
+import com.example.amperewattcalculation.repository.ItemRepository;
+import com.example.amperewattcalculation.services.ItemService;
+
+@Controller
+public class ItemController {
+
+  private final ItemRepository itemrepository;
+
+  @Autowired
+  ItemService item_service;
+
+  public ItemController(ItemRepository itemrepository) {
+    this.itemrepository = itemrepository;
+  }
+
+  @GetMapping("/item/register")
+  public String item_register() {
+    return "Item/register";
+  }
+
+  @PostMapping("/item/register")
+  public String item_register(@ModelAttribute Item item) {
+    item_service.register(item);
+    return "redirect:/device";
+  }
+
+  @GetMapping("/item/edit/{id}")
+  public String item_edit(@PathVariable("id") long item_id, Model model) {
+    // idと一致するデータを取得
+    model.addAttribute("item", itemrepository.findById(item_id));
+    return "Item/edit";
+  }
+
+  @PostMapping("/item/update/{id}")
+  public String item_update(@PathVariable("id") long item_id, @ModelAttribute Item item, Model model) {
+    item_service.update(item, item_id);
+
+    return "redirect:/device";
+  }
+
+  @PostMapping("/item/delete/{id}")
+  public String item_delete(@PathVariable("id") long item_id, Model model) {
+    item_service.delete(item_id);
+
+    return "redirect:/device";
+  }
+
+  // 初期データ
+  @PostConstruct
+  public void dataInit() {
+
+    Item iPhone = new Item();
+    iPhone.setItem_maker_name("Apple");
+    iPhone.setItem_name("iPhone13");
+    iPhone.setItem_code("AI13");
+    iPhone.setAmpere(20);
+    iPhone.setWatt(10);
+    itemrepository.saveAndFlush(iPhone);
+  }
+
+}
---
+++ src/main/java/com/example/amperewattcalculation/controllers/PowerStripController.java
@@ -0,0 +1,77 @@
+package com.example.amperewattcalculation.controllers;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.amperewattcalculation.models.PowerStrip;
+import com.example.amperewattcalculation.repository.PowerStripRepository;
+import com.example.amperewattcalculation.services.PowerStripService;
+
+@Controller
+public class PowerStripController {
+
+  private final PowerStripRepository psrepository;
+
+  @Autowired
+  PowerStripService ps_service;
+
+  public PowerStripController(PowerStripRepository psrepository) {
+    this.psrepository = psrepository;
+  }
+
+  @GetMapping("/powerstrip/register")
+  public String ps_register() {
+    return "PowerStrip/register";
+  }
+
+  @PostMapping("/powerstrip/register")
+  public String ps_register(@ModelAttribute PowerStrip powerstrip, Model model) {
+    ps_service.register(powerstrip);
+    return "redirect:/device";
+  }
+
+  @GetMapping("/powerstrip/edit/{id}")
+  public String ps_edit(@PathVariable("id") long ps_id, Model model) {
+    // idと一致するデータを取得
+    model.addAttribute("powerstrip", psrepository.findById(ps_id));
+    return "PowerStrip/edit";
+  }
+
+  @PostMapping("/powerstrip/update/{id}")
+  public String ps_update(@PathVariable("id") long ps_id, @ModelAttribute PowerStrip powerstrip, Model model) {
+    ps_service.update(powerstrip, ps_id);
+    return "redirect:/device";
+  }
+
+  @PostMapping("/powerstrip/delete/{id}")
+  public String ps_delete(@PathVariable("id") long ps_id, Model model) {
+    ps_service.delete(ps_id);
+    return "redirect:/device";
+  }
+
+  // 初期データ
+  @PostConstruct
+  public void dataInit() {
+    PowerStrip Panasonic = new PowerStrip();
+    Panasonic.setPs_maker_name("Panasonic");
+    Panasonic.setPs_name("電源タップ");
+    Panasonic.setPs_code("P01");
+    Panasonic.setOutllet_number(3);
+    psrepository.saveAndFlush(Panasonic);
+
+    PowerStrip Apw = new PowerStrip();
+    Apw.setPs_maker_name("Apw");
+    Apw.setPs_name("電源タップ");
+    Apw.setPs_code("APW01");
+    Apw.setOutllet_number(3);
+    psrepository.saveAndFlush(Apw);
+  }
+
+}
